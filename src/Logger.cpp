a24c970507cff0e37175fcbf8d83964e2de78b7a
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-06-11 22:27:11 -0500

    suppress 'register' storage class warnings
    Stops the build tools from spamming us with "'register' storage class specifier is deprecated".

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -8,14 +8,17 @@
 
 #include "Logger.h"
 
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-register"
+    #include <boost/log/core.hpp>
+    #include <boost/log/trivial.hpp>
+    #include <boost/log/expressions.hpp>
+    #include <boost/log/sinks/text_file_backend.hpp>
+    #include <boost/log/utility/setup/file.hpp>
+    #include <boost/log/utility/setup/common_attributes.hpp>
+    #include <boost/log/sources/severity_logger.hpp>
+    #include <boost/log/sources/record_ostream.hpp>
+#pragma clang diagnostic pop
 
 using namespace ci;
 
